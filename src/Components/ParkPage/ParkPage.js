0d8af7624bdc3f813eb2af7e389779f47a40f093
Banjamin Hatch <banjaminhatch@gmail.com>
2023-09-21 12:26:54 -0600

    Implement wish list

--- src/Components/ParkPage/ParkPage.js
+++ src/Components/ParkPage/ParkPage.js
@@ -3,6 +3,7 @@ import { Link , useParams} from 'react-router-dom'
 import { useParksContext } from '../../Context/ParksContext';
 import {useState, useEffect} from 'react'
 import ImageView from '../ImageView/ImageView';
+import { FavoritesContext, useFavoritesContext } from '../../Context/FavoritesContext';
 
 
 function ParkPage(){
@@ -10,8 +11,38 @@ function ParkPage(){
     const {stateParkData} = useParksContext();
     const parkId = useParams().parkID;
     const [park, setPark] = useState(() => stateParkData.find((park) => park.id === parkId) || null)
+    const {favorites,toggleFavorites} = useFavoritesContext();
+    const [isWish, setIsWish] = useState(false);
+
+    useState(() => {
+        if(park){
+        console.log("PARK TEST")
+        const result = checkIfFavorite()
+        console.log("RESTUL", result)
+        if(result !== undefined){
+            console.log("WISH TRUE")
+            setIsWish(true)
+        }
+    }
+    },[favorites,park])
+
+    // function addToWishList(){
+    //     setFavorites([...favorites, park])
+    // }
     
-    
+    function checkIfFavorite(){
+        const isFav = favorites.find(favorite => favorite.id === park.id)
+        return isFav
+    }
+
+    function toggleFav(){
+        if(isWish){
+            setIsWish(false)
+        }
+        else{
+            setIsWish(true)
+        }
+    }
 
     const activities =  park && park.activities && park.activities.length > 0 ? (
     
@@ -26,11 +57,15 @@ function ParkPage(){
 
     ) : (<p>No activites</p>)
   
-
+    
     
     return (park ? (
         <div className='park-info-page'>
             <Link to={`/states/${stateID}`}><button className='back-to-map'>Back to Map</button></Link>
+            <button onClick={(e) => {
+                                    toggleFav()
+                                    toggleFavorites(park)}}>{isWish ? 'Remove from wish list' : 'Add to wish List'}
+            </button>
             <h2>{park.fullName}</h2>
             <p>{park.addresses[0].line1}</p>
             <p>{park.addresses[0].city}</p>
